5c32ce2e00ee1648e34587b2ffad643da079e909
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-07-26 15:56:46 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 166 ms, Memory: 61,720 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -0,0 +1,12 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    numbers = [0] * 10
+
+    count = 1
+    while 0 in numbers:
+        num = str(N * count)
+        for i in range(len(num)):
+            numbers[int(num[i])] += 1
+        count += 1
+    print(f'#{test_case} {num}')
