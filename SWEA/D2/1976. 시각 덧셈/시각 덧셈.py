2b8a6e7b5f15e28c20843a6f5bdba1cb054105bb
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-07-26 15:55:31 +0900

    [D2] Title: 시각 덧셈, Time: 155 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -0,0 +1,13 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    H1, M1, H2, M2 = map(int, input().split())
+
+    minute = M1 + M2
+    hour = H1 + H2
+    if minute >= 60:
+        hour += 1
+        minute -= 60
+    if hour >= 13:
+        hour -= 12
+    
+    print(f'#{test_case} {hour} {minute}')
