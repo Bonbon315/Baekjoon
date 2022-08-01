67b04e76d0ecd8061204661621cddbdc29a1a70f
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-08-01 14:33:41 +0900

    [Silver V] Title: 자료구조는 정말 최고야, Time: 312 ms, Memory: 52336 KB -BaekjoonHub

--- 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -1,11 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 answer = True
 for i in range(M):
     bookcount = int(input())
-    bookstack = (list(map(int, input().split())))
+    bookstack = list(map(int, input().split()))
+
     for k in range(bookcount-1):
         if bookstack[k] < bookstack[k+1]:
             answer = False
+
 if answer:
     print('Yes')
 else:
