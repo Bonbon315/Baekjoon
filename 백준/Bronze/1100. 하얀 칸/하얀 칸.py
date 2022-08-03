712aeb82c6a447b654d9d01764e43b9f52086672
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-08-03 17:49:49 +0900

    [Bronze II] Title: 하얀 칸, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,15 +1,9 @@
-import sys
-input = sys.stdin.readline
+chess = [list(input()) for _ in range(8)]
 
-chess = [list(input().rstrip()) for _ in range(8)]
-ans = 0
-for i in range(8):
-    if i % 2 == 0:
-        for j in range(8):
-            if chess[i][j] == 'F' and j % 2 == 0:
-                ans += 1
-    elif i % 2 == 1:
-        for j in range(8):
-            if chess[i][j] == 'F' and j % 2 == 1:
-                ans += 1
-print(ans)
+cnt = 0
+for x in range(8):
+    for y in range(8):  
+        if (x + y) % 2 == 0 and chess[x][y] == 'F':
+            cnt += 1
+
+print(cnt)
