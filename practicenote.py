4b67830297abae303e703fec48800b7133675043
Bonbon315 <n202u123@gmail.com>
2022-08-03 16:54:01 +0900

    p

--- practicenote.py
+++ practicenote.py
@@ -1,16 +1,40 @@
 import sys
-sys.stdin = open('input.txt', 'r', encoding='utf-8')
+sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-N = int(input())
-cnt = 0
-persons = set()
-for i in range(N):
-    s = input().rstrip()
-    if s == 'ENTER':
-        cnt += len(persons)
-        persons = set()
+def prize17(rank):
+    if rank == 1:
+        return 5000000
+    elif 2 <= rank <= 3:
+        return 3000000
+    elif 4 <= rank <= 6:
+        return 2000000
+    elif 7 <= rank <= 10:
+        return 500000
+    elif 11 <= rank <= 15:
+        return 300000
+    elif 16 <= rank <= 21:
+        return 100000
     else:
-        persons.add(s)
-cnt += len(persons)
-print(cnt)
+        return 0
+
+def prize18(rank):
+    if rank == 1:
+        return 5120000
+    elif 2 <= rank <= 3:
+        return 2560000
+    elif 4 <= rank <= 7:
+        return 1280000
+    elif 8 <= rank <= 15:
+        return 640000
+    elif 16 <= rank <= 31:
+        return 320000
+    else:
+        return 0
+
+
+T = int(input())
+for i in range(T):
+    a, b = map(int, input().split())
+    print(prize17(a) + prize18(b))
+
---
+++ practicenote_2.py
@@ -0,0 +1,3 @@
+a = int(input())
+newa = int(str(a).replace('6', '5'))
+print(newa)
