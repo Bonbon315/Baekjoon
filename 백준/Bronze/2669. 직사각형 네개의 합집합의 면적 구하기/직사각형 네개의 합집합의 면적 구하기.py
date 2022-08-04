a64a5198668f6f044a6e521678c5807e2e645907
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-08-04 14:51:09 +0900

    [Bronze I] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
+++ 백준/Bronze/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
@@ -4,10 +4,8 @@ input = sys.stdin.readline
 matrix = [['O']*100 for _ in range(100)]
 for i in range(4):
     x1, y1, x2, y2 = map(int, input().split())
-    x2 -= 1
-    y2 -= 1
-    for i in range(x1, x2+1):
-        for j in range(y1, y2+1):
+    for i in range(x1, x2):
+        for j in range(y1, y2):
             matrix[i][j] = 'X'
 
 area = 0
