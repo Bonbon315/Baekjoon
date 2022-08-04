0303ebd6c1fbd099ed5917db2459a6e7fcf6e7f7
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-08-04 16:00:42 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.py
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.py
@@ -5,5 +5,4 @@ N, M = map(int, input().split())
 bread = [list(input().rstrip()) for _ in range(N)]
 
 for i in range(N):
-    bread[i] = bread[i][::-1]
-    print(*bread[i], sep='')
+    print(*bread[i][::-1], sep='')
