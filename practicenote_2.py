0228045df94644dea29d2b3c13448e5a0be7c08e
Bonbon315 <n202u123@gmail.com>
2022-08-07 23:51:10 +0900

    test

--- practicenote_2.py
+++ practicenote_2.py
@@ -1,3 +1,21 @@
-a = int(input())
-newa = int(str(a).replace('6', '5'))
-print(newa)
+import sys
+input = sys.stdin.readline
+
+
+T = int(input())
+
+for i in range(T):
+    p = list(input())
+    cnt = 0
+    for k in p:
+        if k == '(':
+            cnt += 1
+        if k == ')':
+            cnt -= 1
+        if cnt < 0:
+            print('NO')
+            break
+    if cnt > 0:
+        print('NO')
+    elif cnt == 0:
+        print('YES')
