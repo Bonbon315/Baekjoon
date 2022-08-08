5c02f16a476ef2b14e815f9994a6bd0daebb875e
Bonbon315 <n202u123@gmail.com>
2022-08-08 17:03:26 +0900

    p

--- practicenote.py
+++ practicenote.py
@@ -2,34 +2,28 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-omok = [list(map(int, input().split())) for _ in range(19)]
+N, M = map(int, input().split())
 
-dx = [0, 1, 1, -1]
-dy = [1, 0, 1, 1]
+maze = [list(map(int, input().strip())) for _ in range(N)]
+route = [[0] * M for _ in range(N)]
 
-for x in range(19):
-    for y in range(19):
-        if omok[x][y] != 0:
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-            for i in range(4):
-                cnt = 1
-                nx = x + dx[i]
-                ny = y + dy[i]
+queue = [(0, 0)]
+route[0][0] = 1
 
-                while 0 <= nx < 19 and 0 <= ny < 19 and omok[nx][ny] == omok[x][y]:
-                    cnt += 1
+while queue:
+    x, y = queue.pop(0)
 
-                    if cnt == 5:
-                        if 0 <= x - dx[i] < 19 and 0 <= y - dy[i] < 19 and omok[x - dx[i]][y - dy[i]] == omok[x][y]:
-                            break
-                        if 0 <= nx + dx[i] < 19 and 0 <= ny + dy[i] < 19 and omok[nx + dx[i]][ny + dy[i]] == omok[x][y]:
-                            break
-                        
-                        print(omok[x][y])
-                        print(x + 1, y + 1)
-                        exit()
+    if x == N-1 and y == M-1:
+        print(route[x][y])
+        break
 
-                    nx += dx[i]
-                    ny += dy[i]
-
-print(0)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if route[nx][ny] == 0 and maze[nx][ny] == 1:
+                route[nx][ny] = route[x][y] + 1
+                queue.append((nx,ny))
