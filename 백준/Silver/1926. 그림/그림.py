aa06bc64e95d8b9d6027f3dadbf67291b7b922e2
Bonbon <108643294+Bonbon315@users.noreply.github.com>
2022-08-11 17:01:43 +0900

    [Silver I] Title: 그림, Time: 520 ms, Memory: 33344 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,36 +1,41 @@
+from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def dfs(x, y):
-    global size
-    if x < 0 or y < 0 or x >= n or y >= m:
-        return False
-    if graph[x][y] == 0:
-        return False
-    if graph[x][y] == 1:
-        graph[x][y] = 0
-        size += 1
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-    return False
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-res = []
-size = 0
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    picture[x][y] = 0
+    cnt = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and picture[nx][ny] == 1:
+                picture[nx][ny] = 0
+                queue.append((nx, ny))
+                cnt += 1
+    
+    return cnt
+
+n, m = map(int,input().split())
+picture = [list(map(int, input().split())) for _ in range(n)]
+
+num_list = []
 for i in range(n):
     for j in range(m):
-        if dfs(i, j) == True:
-            res.append(size)
-            size = 0
+        if picture[i][j] == 1:
+            num_list.append(bfs(i,j))
 
-if len(res) == 0:
-    print(len(res))
+if len(num_list) == 0:
+    print(len(num_list))
     print(0)
 else:
-    print(len(res))
-    print(max(res))
+    print(len(num_list))
+    print(max(num_list))
