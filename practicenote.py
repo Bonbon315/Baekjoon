fbc4505de8abdfbccfdf8bfc53f6c5000c5fb056
Bonbon315 <n202u123@gmail.com>
2022-08-27 01:45:32 +0900

    p

--- practicenote.py
+++ practicenote.py
@@ -1,32 +1,35 @@
 import sys
 sys.stdin = open('input.txt')
-sys.setrecursionlimit(5000)
 input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0, -1, 1, -1, 1] #상하좌우 오위/오아래/왼위/왼아래
-dy = [0, 0, -1, 1, 1, 1, -1, -1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def dfs(x, y):
-    visited[x][y] = 1
-    for i in range(8): #델타탐색
+    graph[x][y] = 0 
+    global cnt
+    cnt += 1
+    for i in range(4): 
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < h and 0 <= ny < w and island[nx][ny] == 1 and not visited[nx][ny]:
-            dfs(nx,ny) 
+        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+            dfs(nx,ny)
 
 
-while True:
-    w, h = map(int, input().split()) # w = y / h = x
-    if w == 0 and h == 0:
-        break
-    
-    island = [list(map(int, input().split())) for _ in range(h)]
-    ans = 0
-    visited = [[0] * w for _ in range(h)]
-    for i in range(h):
-        for j in range(w):
-            if island[i][j] == 1 and not visited[i][j]:
-                dfs(i, j)
-                ans += 1
-    print(ans)
+n = int(input())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+ans = 0
+anslist = []
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            cnt = 0
+            dfs(i, j)
+            anslist.append(cnt)
+            ans += 1
+anslist.sort()
+print(ans)
+for i in range(ans):
+    print(anslist[i])
