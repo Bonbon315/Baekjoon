be1ab6cc4349eda9b9b0f270ddd2c63178d6b8df
Bonbon315 <n202u123@gmail.com>
2022-09-26 13:45:42 +0900

    black installed

--- practicenote.py
+++ practicenote.py
@@ -1,35 +1,15 @@
 import sys
-sys.stdin = open('input.txt')
+
+sys.stdin = open("input.txt")
 input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-def dfs(x, y):
-    graph[x][y] = 0 
-    global cnt
-    cnt += 1
-    for i in range(4): 
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
-            dfs(nx,ny)
+def fib(n):
+    num, nextnum = 0, 1
+    for _ in range(n):
+        num, nextnum = nextnum, num + nextnum
+    return num
 
 
 n = int(input())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
-ans = 0
-anslist = []
-
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            cnt = 0
-            dfs(i, j)
-            anslist.append(cnt)
-            ans += 1
-anslist.sort()
-print(ans)
-for i in range(ans):
-    print(anslist[i])
+print(fib(n))
